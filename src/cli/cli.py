7db3f1ff72e4289a31325014895cc2be2f161c6b
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-07-30 09:20:15 +0330

    cli.py and service.py Updated

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -78,7 +78,23 @@ try:
                     raise getopt.error("too many arguments")
                 else:
                     api_key = currentArgs
-                    src.db.db.import_gpt_api_key(api_key)
+                    check = src.db.db.check_if_api_key_exists()
+                    if check:
+                        user_answer = input(
+                            "API Key Exists - Want to Replace this API Key with the Old One? [y/n] "
+                        )
+                        while True:
+                            match user_answer:
+                                case "y" | "Y":
+                                    src.db.db.clean_gpt_table()
+                                    src.db.db.import_gpt_api_key(api_key)
+                                    break
+                                case "n" | "N":
+                                    sys.exit()
+                                case _:
+                                    user_answer = input("Please Answer with Y or N: [y/n] ")
+                    else:
+                        src.db.db.import_gpt_api_key(api_key)
 
             # Show Handler
             case "--show":
