671a5bb637cf58db3c6fb9880c780e45b320536d
Hanif <hanif@Hanifs-MacBook-Pro.local>
2024-08-07 14:32:19 +0330

    main.py -> runnable script

--- shellsensei/cli/main.py
+++ shellsensei/cli/main.py
@@ -5,147 +5,149 @@ from shellsensei.cli import service
 from shellsensei.db import db
 
 
-# Arguments - Remove 1st Argument from the List
-argsList = sys.argv[1:]
+if __name__ == "__main__":
 
-# Short Options
-shortopts = "hvm:"
+    # Arguments - Remove 1st Argument from the List
+    argsList = sys.argv[1:]
 
-# Long Options
-longopts = ["help", "version", "msg=", "config=", "import=", "show=", "remove"]
+    # Short Options
+    shortopts = "hvm:"
 
-try:
-    # Parsing Argument(s)
-    opts, values = getopt.getopt(argsList, shortopts, longopts)
+    # Long Options
+    longopts = ["help", "version", "msg=", "config=", "import=", "show=", "remove"]
 
-    # Checking Each Arguments
-    for currentOpts, currentArgs in opts:
+    try:
+        # Parsing Argument(s)
+        opts, values = getopt.getopt(argsList, shortopts, longopts)
 
-        # Switch Case currentOpts Variable
-        match currentOpts:
+        # Checking Each Arguments
+        for currentOpts, currentArgs in opts:
 
-            # Help Handler
-            case "-h" | "--help":
-                if len(argsList) != 1:
-                    raise getopt.error("too many arguments")
-                else:
-                    service.show_help()
+            # Switch Case currentOpts Variable
+            match currentOpts:
 
-            # Version Handler
-            case "-v" | "--version":
-                if len(argsList) != 1:
-                    raise getopt.error("too many arguments")
-                else:
-                    service.show_version()
+                # Help Handler
+                case "-h" | "--help":
+                    if len(argsList) != 1:
+                        raise getopt.error("too many arguments")
+                    else:
+                        service.show_help()
 
-            # Message Handler
-            case "-m" | "--msg":
-                msg = currentArgs
-                service.output(msg)
+                # Version Handler
+                case "-v" | "--version":
+                    if len(argsList) != 1:
+                        raise getopt.error("too many arguments")
+                    else:
+                        service.show_version()
 
-            # Config Handler
-            case "--config":
-                # Switch Case on currentArgs Variable
-                match currentArgs:
-                    case "init":
-                        if len(argsList) != 2:
-                            raise getopt.error("too many arguments")
+                # Message Handler
+                case "-m" | "--msg":
+                    msg = currentArgs
+                    service.output(msg)
+
+                # Config Handler
+                case "--config":
+                    # Switch Case on currentArgs Variable
+                    match currentArgs:
+                        case "init":
+                            if len(argsList) != 2:
+                                raise getopt.error("too many arguments")
+                            else:
+                                db.init()
+                        case "location":
+                            if len(argsList) != 2:
+                                raise getopt.error("too many arguments")
+                            else:
+                                service.show_location()
+                        case "rm" if values[0] == "cmd" and len(values) == 1:
+                            db.clean_cmd_table()
+                        case "rm" if values[0] == "prompt" and len(values) == 1:
+                            print("dadas")
+                            db.clean_prompt_table()
+                        case "reset":
+                            if len(argsList) != 2:
+                                raise getopt.error("too many arguments")
+                            db.reset()
+                    # Error Handling for "Too Many Arguments"
+                    if len(values) > 1:
+                        raise getopt.error("too many arguments")
+                    # Error Handling for "Wrong Arguments"
+                    elif currentArgs not in ("init", "location", "rm", "reset"):
+                        raise getopt.error("wrong argument")
+
+                # Import Handler
+                case "--import":
+                    if len(values) != 0:
+                        raise getopt.error("too many arguments")
+                    else:
+                        api_key = currentArgs
+                        check = db.check_if_api_key_exists()
+                        if check:
+                            user_answer = input(
+                                "API Key Exists - Want to Replace this API Key with the Old One? [y/n] "
+                            )
+                            while True:
+                                match user_answer:
+                                    case "y" | "Y":
+                                        db.clean_gpt_table()
+                                        db.import_gpt_api_key(api_key)
+                                        break
+                                    case "n" | "N":
+                                        sys.exit()
+                                    case _:
+                                        user_answer = input(
+                                            "Please Answer with Y or N: [y/n] "
+                                        )
                         else:
-                            db.init()
-                    case "location":
-                        if len(argsList) != 2:
-                            raise getopt.error("too many arguments")
-                        else:
-                            service.show_location()
-                    case "rm" if values[0] == "cmd" and len(values) == 1:
-                        db.clean_cmd_table()
-                    case "rm" if values[0] == "prompt" and len(values) == 1:
-                        print("dadas")
-                        db.clean_prompt_table()
-                    case "reset":
-                        if len(argsList) != 2:
-                            raise getopt.error("too many arguments")
-                        db.reset()
-                # Error Handling for "Too Many Arguments"
-                if len(values) > 1:
-                    raise getopt.error("too many arguments")
-                # Error Handling for "Wrong Arguments"
-                elif currentArgs not in ("init", "location", "rm", "reset"):
-                    raise getopt.error("wrong argument")
+                            db.import_gpt_api_key(api_key)
 
-            # Import Handler
-            case "--import":
-                if len(values) != 0:
-                    raise getopt.error("too many arguments")
-                else:
-                    api_key = currentArgs
-                    check = db.check_if_api_key_exists()
-                    if check:
+                # Show Handler
+                case "--show":
+                    # Switch Case on currentArgs Variable
+                    match currentArgs:
+                        # CMD
+                        case "cmd" if len(values) == 0:
+                            limit = 1
+                            db.show_cmd_table(limit)
+                        case "cmd" if len(values) == 1:
+                            limit = int(values[0])
+                            db.show_cmd_table(limit)
+                        # Prompt
+                        case "prompt" if len(values) == 0:
+                            limit = 1
+                            db.show_prompt_table(limit)
+                        case "prompt" if len(values) == 1:
+                            limit = int(values[0])
+                            db.show_prompt_table(limit)
+                    # Error Handling for "Too Many Arguments"
+                    if len(values) > 1:
+                        raise getopt.error("too many arguments")
+                    # Error Handling for "Wrong Arguments"
+                    elif currentArgs not in ("cmd", "prompt"):
+                        raise getopt.error("wrong argument")
+
+                # Remove Handler
+                case "--remove":
+                    if len(argsList) != 1:
+                        raise getopt.error("too many arguments")
+                    else:
                         user_answer = input(
-                            "API Key Exists - Want to Replace this API Key with the Old One? [y/n] "
+                            "Are you Sure you want to Delete your GPT API Key? [y/n] "
                         )
                         while True:
                             match user_answer:
                                 case "y" | "Y":
                                     db.clean_gpt_table()
-                                    db.import_gpt_api_key(api_key)
                                     break
                                 case "n" | "N":
                                     sys.exit()
                                 case _:
-                                    user_answer = input(
-                                        "Please Answer with Y or N: [y/n] "
-                                    )
-                    else:
-                        db.import_gpt_api_key(api_key)
-
-            # Show Handler
-            case "--show":
-                # Switch Case on currentArgs Variable
-                match currentArgs:
-                    # CMD
-                    case "cmd" if len(values) == 0:
-                        limit = 1
-                        db.show_cmd_table(limit)
-                    case "cmd" if len(values) == 1:
-                        limit = int(values[0])
-                        db.show_cmd_table(limit)
-                    # Prompt
-                    case "prompt" if len(values) == 0:
-                        limit = 1
-                        db.show_prompt_table(limit)
-                    case "prompt" if len(values) == 1:
-                        limit = int(values[0])
-                        db.show_prompt_table(limit)
-                # Error Handling for "Too Many Arguments"
-                if len(values) > 1:
-                    raise getopt.error("too many arguments")
-                # Error Handling for "Wrong Arguments"
-                elif currentArgs not in ("cmd", "prompt"):
-                    raise getopt.error("wrong argument")
-
-            # Remove Handler
-            case "--remove":
-                if len(argsList) != 1:
-                    raise getopt.error("too many arguments")
-                else:
-                    user_answer = input(
-                        "Are you Sure you want to Delete your GPT API Key? [y/n] "
-                    )
-                    while True:
-                        match user_answer:
-                            case "y" | "Y":
-                                db.clean_gpt_table()
-                                break
-                            case "n" | "N":
-                                sys.exit()
-                            case _:
-                                user_answer = input("Please Answer with Y or N: [y/n] ")
-except getopt.error as err:
-    # Output Error, and Return with an Error Code
-    print(str(err))
-    print("please use sensei --help or sensei -h")
-except ValueError as ve:
-    # ValueError
-    print("invalid value, please enter int number -", str(ve))
-    print("please use sensei --help or sensei -h")
+                                    user_answer = input("Please Answer with Y or N: [y/n] ")
+    except getopt.error as err:
+        # Output Error, and Return with an Error Code
+        print(str(err))
+        print("please use sensei --help or sensei -h")
+    except ValueError as ve:
+        # ValueError
+        print("invalid value, please enter int number -", str(ve))
+        print("please use sensei --help or sensei -h")
