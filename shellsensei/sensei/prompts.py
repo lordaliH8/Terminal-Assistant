6680519e003da525b0f49bddb79e62be36fc87a0
lordaliH8 <ali.alnaghipour78@gmail.com>
2024-08-09 21:46:37 +0330

    add scenario|modify sensei|modify 4_test*.py|modify prompts

--- shellsensei/sensei/prompts.py
+++ shellsensei/sensei/prompts.py
@@ -1,8 +1,10 @@
-decision_prompt = """You are sitting behind the terminal of a {OS} machine. You are told to do: {task} You have 
+decision_prompt = """You are a terminal assistant agent(TA).You are sitting behind the terminal of a {OS} machine. You are told to do: {task} You have 
 complete freedom and you can do anything. Here are your options:
-1. You can decide to run a command in terminal: If you choose this, the you will be provided with the output of your command
+1. You can decide to run a command in terminal: If you choose this, the you will be provided with the output of your command from user (user)
 2. You can decide to ask a question: If anything is unclear, or you need more information, you can ask a question from the user. You will be provided with the response.
 
+you have to continue the scenario below based on your capabilities and options :
+{chat_scenario}
 Notes:
 - You must do the task iteratively. For example, if you are asked to install docker, you should not output a 
 series of commands which install the docker. Instead you should only output the first command.
--- tests/4_test_history.py
+++ tests/4_test_history.py
@@ -3,21 +3,27 @@ from shellsensei.sensei import Sensei
 sensei = Sensei(sensei_id="test")
 print(sensei.created_at)
 
-task = "install docker"
-response = sensei.ask(task)
-res =("command response")
+sensei.task = "delete container"
+sensei.scenario =sensei.generate_chat_scenario(sensei.question_history,sensei.command_history)
+
+response = sensei.ask()
+
+res =("""Command 'docker' not found, but can be installed with:
+sudo snap install docker         # version 24.0.5, or
+sudo apt  install docker.io      # version 24.0.5-0ubuntu1
+sudo apt  install podman-docker  # version 4.9.3+ds1-1ubuntu0.1
+See 'snap info docker' for additional versions.
+""")
+
 if response.get("command"):
     sensei.generate_history(response.get("command") , res, type="command")
-
-print("model response is : " , response)
-print("command history content : " , sensei.command_history)
-
-print("%" * 50)
-
-task = "delete container"
-response = sensei.ask(task)
-res = "ubuntu:latest"
 if response.get("question"):
-    sensei.generate_history(response.get("question"), res, type="question")
-print("model response is : " , response)
-print("question history content : " , sensei.question_history)
+    sensei.generate_history(response.get("question") , res, type="question")
+
+sensei.scenario = sensei.generate_chat_scenario(sensei.command_history,sensei.question_history)
+
+response =sensei.ask()
+
+print(response)
+
+
