55a28b6892a80062c5bfb596c5f7b60b7e31700a
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-08-09 17:57:34 +0330

    prompt.py Updated

--- shellsensei/cli/services/prompt/prompt.py
+++ shellsensei/cli/services/prompt/prompt.py
@@ -1,9 +1,14 @@
 # Imports
+import os
+import sys
 import getopt
+from shellsensei.sensei import Sensei
+
+sensei = Sensei(sensei_id="test")
 
 
 # Prompt Handler
-def prompt_handler(currentArgs, values, sensei):
+def prompt_handler(currentArgs, values):
     if len(values) != 0:
         raise getopt.error("too many arguments")
     else:
@@ -11,14 +16,41 @@ def prompt_handler(currentArgs, values, sensei):
         response = sensei.ask(msg)
         print(response)
         while True:
+            # Question
             if response["question"]:
                 print(response["question"])
+                response_type = "question"
                 user_answer = input(response["question"] + " ")
                 print(user_answer)
+                sensei.generate_history(
+                    key=response["question"], value=user_answer, type=response_type
+                )
+                print("question history ~>", sensei.question_history)
                 break
+            # Command
             elif response["command"]:
+                response_type = "command"
                 print(response["command"])
-                # service.execute(response["command"])
-                break
+                sensei.generate_history(
+                    key=response["command"], value="OK", type=response_type
+                )
+                print("command history ~>", sensei.command_history)
+                # execute(response["command"])
+                user_answer = input("You want to Ask another Request? [y/n] ")
+                while True:
+                    match user_answer:
+                        case "y" | "Y":
+                            user_prompt = input("Your Request ~> ")
+                            response = sensei.ask(user_prompt)
+                            break
+                        case "n" | "N":
+                            sys.exit()
+                        case _:
+                            user_answer = input("Please Answer with Y or N: [y/n] ")
             else:
                 raise getopt.error("WTF")
+
+
+# Execute Shell Command
+def execute(command):
+    os.system(command)
