eb0140fb72e34614d9ef4cb8b83fbcdf9d00ca18
Hanif <hanif@Hanifs-MacBook-Pro.local>
2024-08-09 18:14:41 +0330

    add dynamic cache directory

--- shellsensei/cli/services/config/config.py
+++ shellsensei/cli/services/config/config.py
@@ -1,11 +1,16 @@
 # Imports
 import getopt
 from shellsensei.db import db
+import os
 
 
 # Show DataBase and Config File Location
-def show_location():
-    print("~/.local/share/shellsensei/")
+def get_location():
+    home_dir = os.path.expanduser("~")
+    config_dir = os.path.join(home_dir, ".shellsensei")
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    return config_dir
 
 
 # Config Handler
@@ -16,12 +21,12 @@ def config_handler(argsList, currentArgs, values):
             if len(argsList) != 2:
                 raise getopt.error("too many arguments")
             else:
-                db.init()
+                db.init(folder=get_location())
         case "location":
             if len(argsList) != 2:
                 raise getopt.error("too many arguments")
             else:
-                show_location()
+                print(get_location())
         case "rm" if values[0] == "cmd" and len(values) == 1:
             db.clean_cmd_table()
         case "rm" if values[0] == "prompt" and len(values) == 1:
@@ -30,6 +35,7 @@ def config_handler(argsList, currentArgs, values):
             if len(argsList) != 2:
                 raise getopt.error("too many arguments")
             db.reset()
+
     # Error Handling for "Too Many Arguments"
     if len(values) > 1:
         raise getopt.error("too many arguments")
