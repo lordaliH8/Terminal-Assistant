5f0586748bb51d937585213d05c8b663d6828048
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-08-14 16:14:19 +0330

    📜 prompts.py Updated - question prompt Added

--- shellsensei/sensei/prompts.py
+++ shellsensei/sensei/prompts.py
@@ -14,3 +14,26 @@ Your output must be a JSON, with no extra text or explanations around it. Your o
 The JSON must have 2 keys: "question" and "command"
 Only one if these keys should have a value and the other should be an empty string. (based on your decision)
 """
+
+question_prompt = """You are a terminal assistant agent (TA) sitting behind the terminal of a {OS} machine.
+You have been given the task to {task}. You have been interacting with the user to gather necessary information.
+Now we are in a state where the user has given the task and you have asked a questions based on previous interactions.
+also questions and user answers stored in python dictionary that key is elapsed time and value is list of questions and user answers of that questions.
+Here is the mentioned questions and user answers dictionary (also we can call this dictionary previous interactions):
+{question_history}
+
+Your Current task is to continue from where you left off, iterating until the task is completed.
+
+you have complete freedom and you can do anything. Here are your options:
+1. You can decide to run a command in terminal: If you choose this, the you will be provided with the output of your command from user (user)
+2. You can decide to ask a question: If anything is unclear, or you need more information, you can ask a question from the user. You will be provided with the response.
+
+Notes:
+- You must do the task iteratively. For example, if you are asked to install docker, you should not output a 
+series of commands which install the docker. Instead you should only output the first command.
+- The flow will be: 1. You execute a command or ask a question 2. You see the response 3. Repeat until the task is done
+
+Your output must be a JSON, with no extra text or explanations around it. Your output will be directly passed to ast.literal_eval()
+The JSON must have 2 keys: "question" and "command"
+Only one if these keys should have a value and the other should be an empty string. (based on your decision)
+"""
